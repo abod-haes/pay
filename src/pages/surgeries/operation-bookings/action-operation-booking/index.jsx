5c4460d8e0cc57d481771fb4348bfbb5a01849d0
Abod_Hares <133676503+abod-haes@users.noreply.github.com>
2026-06-16 00:47:22 +0300

    fix: move operation archive below operation details

--- src/pages/surgeries/operation-bookings/action-operation-booking/index.jsx
+++ src/pages/surgeries/operation-bookings/action-operation-booking/index.jsx
@@ -287,31 +287,29 @@ const ActionOperationBooking = () => {
       />
       <div>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-          <div className="flex flex-col lg:flex-row gap-[20px]">
-            <div className="space-y-6 w-full lg:w-[80%] relative">
-              <LoadingSection isLoading={isLoading || isRefetching} otherStyle={"h-full"} />
-              <PatientInformation
-                control={control}
-                watch={watch}
-                errors={errors}
-                setValue={setValue}
-                didInit={didInit}
-                reset={reset}
-                dirtyFields={dirtyFields}
-              />
-              <MedicalInformation control={control} setValue={setValue} watch={watch} />
-              <ProcessData control={control} setValue={setValue} watch={watch} errors={errors} />
-              <DateAndTime
-                control={control}
-                setValue={setValue}
-                watch={watch}
-                dataToReset={data?.data?.data}
-                isSelected={isSelected}
-                setIsSelected={setIsSelected}
-                selectDate={selectDate}
-                setSelectedDate={setSelectedDate}
-              />
-            </div>
+          <div className="relative space-y-6">
+            <LoadingSection isLoading={isLoading || isRefetching} otherStyle={"h-full"} />
+            <PatientInformation
+              control={control}
+              watch={watch}
+              errors={errors}
+              setValue={setValue}
+              didInit={didInit}
+              reset={reset}
+              dirtyFields={dirtyFields}
+            />
+            <MedicalInformation control={control} setValue={setValue} watch={watch} />
+            <ProcessData control={control} setValue={setValue} watch={watch} errors={errors} />
+            <DateAndTime
+              control={control}
+              setValue={setValue}
+              watch={watch}
+              dataToReset={data?.data?.data}
+              isSelected={isSelected}
+              setIsSelected={setIsSelected}
+              selectDate={selectDate}
+              setSelectedDate={setSelectedDate}
+            />
             <SurgeriesArchaive
               control={control}
               files={attachmentsFiles}
